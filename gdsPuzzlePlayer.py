13af4ab81c0a5c269752ad5cc9590ca1cbc41410
bullbin <34871082+bullbin@users.noreply.github.com>
2019-06-21 09:10:29 +0100

    Update draw routines for some text and faders
    f a d e d

--- gdsPuzzlePlayer.py
+++ gdsPuzzlePlayer.py
@@ -3,34 +3,65 @@
 import gdsLib, pygame
 
 pygame.init()
-pathLaytonAssetRoot = r"C:\Users\USERNAME\Documents\GitHub\curious_village\assets\\"
+pathLaytonAssetRoot = r"C:\Users\USERNAME\Documents\GitHub\layton1\assets\\"
+
+class AnimatedImage():
+        self.image = pygame.image.load(pathLaytonAssetRoot + imagePath)
+        self.pos = (x,y)
+    def draw(self, gameDisplay):
+        gameDisplay.blit(self.image, self.pos)
+
+class Fader():
+    def __init__(self, colour):
+        self.drawIncomplete = True
+    def update(self):
+        if self.drawIncomplete:
+            pass
 
 class TextScroller():
     def __init__(self, textInput):
         self.textInput = textInput
+        self.lastNewline = 0
         self.textPos = 1
         self.drawIncomplete = True
-    def update(self):
+        self.font = pygame.font.Font('freesansbold.ttf', 12)
+        self.text = [self.font.render(self.textInput[0:self.textPos], True, (0,255,0), (0,0,255))]
+
+    def draw(self, gameDisplay):
         if self.drawIncomplete:
             if self.textPos <= len(self.textInput):
                 self.textPos += 1
-        return self.textInput[0:self.textPos]
+            if self.textInput[self.textPos - 2] == "\n":
+                self.text[-1] = self.font.render(self.textInput[self.lastNewline:self.textPos - 2], True, (0,255,0), (0,0,255))
+                print(self.textInput[self.lastNewline:self.textPos - 1])
+                self.text.append(self.font.render(self.textInput[self.lastNewline + 1:self.textPos], True, (0,255,0), (0,0,255)))
+                self.lastNewline = self.textPos - 1
+            else:
+                self.text[-1] = self.font.render(self.textInput[self.lastNewline:self.textPos], True, (0,255,0), (0,0,255))
+
+        y = 0
+        for text in self.text:
+            textRect = text.get_rect()
+            centX, centY = textRect.center
+            centY += y
+            textRect.center = (centX, centY)
+            gameDisplay.blit(text, textRect)
+            y += 12
 
 class LaytonPuzzle():
     def __init__(self, puzzleIndex):
         self.puzzleIndex = puzzleIndex
-        self.background = pygame.image.load(pathLaytonAssetRoot + "bg\en\q_bg.png")
+        self.backgroundTs = pygame.image.load(pathLaytonAssetRoot + "bg\en\q_bg.png")
+        self.backgroundBs = pygame.image.load(pathLaytonAssetRoot + "bg\q" + str(self.puzzleIndex) + "_bg.png")
         with open(pathLaytonAssetRoot + r"qtext\en\q000\q_" + str(self.puzzleIndex) + ".txt", 'r') as qText:
             self.backgroundText = TextScroller(qText.read())
 
-        print(self.backgroundText.textInput)
         self.textDrawIncomplete = True
 
     def draw(self, gameDisplay):
-        if self.backgroundText.drawIncomplete:
-            print(self.backgroundText.update())
-            pass
-        gameDisplay.blit(self.background, (0,0))
+        self.backgroundText.draw(gameDisplay)
+        #gameDisplay.blit(self.backgroundTs, (0,0))
+        #gameDisplay.blit(self.backgroundBs, (0,192))
 
 class PuzzleSliding(LaytonPuzzle):
     def __init__(self, puzzleIndex):
@@ -53,28 +84,60 @@ class LaytonPuzzlePlayer():
         self.puzzle = None
         for command in self.script.commands:
             if command.opcode == b'\x1b':
-                if command.operands[0] in ["Draw Input2"]:
+                if command.operands[0] in ["Draw Input2", "Match"]:
                     self.puzzle = PuzzleDrawInput2(puzzleIndex)
                 else:
                     print(command.operands[0])
         
         self.clock = pygame.time.Clock()
+        self.currentFrameStep = 0
+
+    def loadIntroSequence(self):
+        self.backgroundTs = AnimatedImage(r"\bg\en\judge_l114_bg.png")
+        self.backgroundBs = AnimatedImage(r"\bg\en\picarat_bg.png")
+        self.backgroundTs.pos = (0, 192)
+        self.backgroundBs.pos = (0, 192)
+
+    def update(self, drawIntro, drawIntroComplete):
         
+        if self.puzzle != None:
+            if drawIntro:
+                resX, resY = self.backgroundTs.pos
+                if resY > 0:
+                    resY -= 12
+                    self.backgroundTs.pos = (resX, resY)
+                else:
+                    drawIntroComplete = True
+                    
+                self.backgroundBs.draw(self.gameDisplay)
+                self.backgroundTs.draw(self.gameDisplay)
+
+                if drawIntroComplete:
+                    drawIntro = False
+
+            else:
+                self.puzzle.draw(self.gameDisplay)
+            pygame.display.update()
+    
     def play(self):
         isActive = True
+        
+        drawIntro = False
+        drawIntroComplete = False
+        
+        if drawIntro:
+            self.loadIntroSequence()
+        
         while isActive:
+            
+            self.update(drawIntro, drawIntroComplete)
+            
             for event in pygame.event.get():
+                
                 if event.type == pygame.QUIT:
                     isActive = False
-
-                if self.puzzle != None:
-                    self.puzzle.draw(self.gameDisplay)
-                    pygame.display.update()
-                    self.clock.tick(60)
-                else:
-                    pygame.display.quit()
-                    pygame.quit()
-                    isActive = False
                     
-puzzleInstance = LaytonPuzzlePlayer(6)
+            self.clock.tick(60)
+                    
+puzzleInstance = LaytonPuzzlePlayer(25)
 puzzleInstance.play()
